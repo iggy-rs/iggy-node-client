f9bd44204f86f2becfe674c9b38f09657c00ac6c
T1B0 <t1b0.gm@gmail.com>
2024-01-31 23:30:42 +0100

    fix: fix tcp client options

--- src/client/tcp.client.ts
+++ src/client/tcp.client.ts
@@ -1,32 +1,35 @@
 
+import { createConnection, Socket, TcpSocketConnectOpts } from 'node:net';
 import type { CommandResponse, Client } from './client.type.js';
-import { createConnection, Socket } from 'node:net';
 import { wrapSocket, sendCommandWithResponse } from './client.utils.js';
 
 
-export const createTcpSocket = (
-  host: string, port: number, keepAlive = true
-): Promise<Socket> => {
-  const socket = createConnection(port, host);
-  socket.setKeepAlive(keepAlive);
+export const createTcpSocket = (options: TcpSocketConnectOpts): Promise<Socket> => {
+  const socket = createConnection(options);
   return wrapSocket(socket);
 };
 
 
-export type TcpOption = {
-  host: string,
-  port: number,
-  keepAlive?: boolean
-};
-
+export type TcpOption = TcpSocketConnectOpts;
 
 export const TcpClient = ({ host, port, keepAlive = true }: TcpOption): Client => {
   let socket: Socket;
   return {
     sendCommand: async (code: number, payload: Buffer):Promise<CommandResponse> => {
       if (!socket)
-        socket = await createTcpSocket(host, port, keepAlive);
+        socket = await createTcpSocket({host, port, keepAlive});
       return sendCommandWithResponse(socket)(code, payload);
     }
   }
 };
+
+// export const createTransport = (options: TcpOption): Client => {
+//   let socket: Socket;
+//   return {
+//     sendCommand: async (code: number, payload: Buffer):Promise<CommandResponse> => {
+//       if (!socket)
+//         socket = await createTcpSocket(options);
+//       return sendCommandWithResponse(socket)(code, payload);
+//     }
+//   }
+// };
